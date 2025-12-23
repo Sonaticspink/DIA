b9caf51f8eec12049c005a877dfb405c7f38df3f
StrangerZ667 <kanchainack123@gmail.com>
2025-12-23 20:53:47 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clinicApp',
+  webDir: 'dist'
+};
+
+export default config;
