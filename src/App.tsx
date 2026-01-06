85010e7af008ba0ac96ab06264f1c83eea5aa8a2
StrangerZ667 <kanchainack123@gmail.com>
2026-01-06 22:29:20 +0700

    View App

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import Appointment from './pages/Appointment';
 import AppointmentDetail from './pages/AppointmentDetail';
 import ReservationPage from './pages/ReservationPage';
 import SuccessPage from './pages/SuccessPage';
+import AppointmentList from './pages/AppointmentList';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -46,6 +47,9 @@ const App: React.FC = () => (
         </Route>
         <Route exact path="/inventory">
           <DrugInventory />
+        </Route>
+        <Route exact path="/appointment-list">
+          <AppointmentList />
         </Route>
         <Route exact path="/">
           <Redirect to="/home" />
